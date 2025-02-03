0a4ffddb029aaf26b3e15a722286c8d71268e891
Olivia Polos <38380599+odp31@users.noreply.github.com>
2025-02-03 14:17:10 -0500

    Create Transaction.java

---
+++ Transaction.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Transaction {
+  public String sender;
+  public String recipient;
+  public float value;
+
+  public Transaction(String from, String to, float value) {
+    this.sender = from;
+    this.recipient = to;
+    this.value = value;
+  }
+}
